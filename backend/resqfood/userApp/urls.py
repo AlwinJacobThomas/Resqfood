dce4160daca1535e707987a6c71342d975e6f213
AlwinJacobThomas <kailath.ajt@gmail.com>
2024-03-27 01:11:15 +0530

    added Login,Logout,Logoutall,Changepassword

--- backend/resqfood/userApp/urls.py
+++ backend/resqfood/userApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 
-from .views import UserRegistration
+from knox import views as knox_views
+from .views import user_registration,user_login,change_password
 # REST API routes 
 
 
@@ -9,5 +10,10 @@ app_name ='userApp'
 urlpatterns = [
     # path('', index.views,name='index'),
    
-    path('register/',UserRegistration.as_view(),name='register')
+    path('register/',user_registration ,name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/',knox_views.LogoutView.as_view(),name='logout'),
+    path('logoutall/',knox_views.LogoutAllView.as_view(),name='logout-all'),  #logout from all browsers
+    path('changepassword/',change_password,name='change-password')
+    # path('logout/', user_logout, name='logout'),
 ]
--- backend/resqfood/userApp/views.py
+++ backend/resqfood/userApp/views.py
@@ -1,9 +1,13 @@
-
+from django.contrib.auth import login,logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from .serializers import UserRegistrationSerializer
+from rest_framework import status,permissions,generics #generics for change pswd
+
+from rest_framework.authtoken.serializers import AuthTokenSerializer #Authentication Token
+from .models import User
+from .serializers import UserRegistrationSerializer,ChangePasswordSerializer
 from knox.models import AuthToken
+from knox.views import LoginView as KnoxLoginView
 
 # Create your views here.
 
@@ -21,3 +25,47 @@ class UserRegistration(APIView):
                 'token' : token[1] 
             })
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+user_registration = UserRegistration.as_view()
+
+class UserLogin(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def post(self,request,format=None):
+        
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request,user)
+        return super(UserLogin,self).post(request,format=None)
+user_login = UserLogin.as_view()
+
+class ChangePassword(generics.UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (permissions.IsAuthenticated,)
+    
+    def get_object(self,querset=None):
+        obj =self.request.user
+        return obj
+    
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        
+        if serializer.is_valid():
+            #check old pswd
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password":["Wrong Password,"]},status=status.HTTP_400_BAD_REQUEST)
+            #set new pswd
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code':status.HTTP_200_OK,
+                'message':'Password updated Sucessfully',
+                'data':[]
+            }
+            return Response(response)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
+            
+change_password = ChangePassword.as_view()      
